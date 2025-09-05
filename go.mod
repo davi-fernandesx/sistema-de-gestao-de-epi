8e174c7351723a28752f8c4c6249ebab6ee8c552
davi <davifernadesr@gmail.com>
2025-09-05 18:51:02 -0300

    atualiza senha do SQL Server no docker-compose e adiciona impressão do hash da senha no main.go

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/davi-fernandesx/sistema-de-gestao-de-epi
 go 1.22.2
 
 require (
+	github.com/denisenkom/go-mssqldb v0.12.3
 	github.com/gin-gonic/gin v1.10.1
 	github.com/joho/godotenv v1.5.1
 )
@@ -12,7 +13,6 @@ require (
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
-	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
--- main.go
+++ main.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
-	"github.com/davi-fernandesx/sistema-de-gestao-de-epi/configs"
-	"github.com/gin-gonic/gin"
+	"github.com/davi-fernandesx/sistema-de-gestao-de-epi/auth"
+	"github.com/davi-fernandesx/sistema-de-gestao-de-epi/model"
 )
 
 func main(){
 
 
 	
-	router:= gin.Default()
+	//router:= gin.Default()
 
-	_, err:= configs.InitAplicattion()
+	//_, err:= configs.InitAplicattion()
+	//if err != nil {
+
+	//	log.Fatal(err)
+    //	}
+
+
+	//router.Run(":8080")
+
+	a:= model.Login{
+		Nome: "davi",
+		Senha: "uee4n43jkj",
+	}
+
+	fmt.Println(a.Nome, a.Senha)
+
+	h, err:= auth.HashPassword(a.Senha)
 	if err != nil {
-
 		log.Fatal(err)
 	}
 
+	i:= fmt.Sprintf("%x", h)
 
-	router.Run(":8080")
+	fmt.Println("hash", i)
+
+	
+
 
 }
