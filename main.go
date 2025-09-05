e4c84d8571151ec9cc5bec238a113695d752d9b3
davi <davifernadesr@gmail.com>
2025-09-05 19:01:44 -0300

    remove variáveis de ambiente fixas do docker-compose e ajusta inicialização no main.go

--- main.go
+++ main.go
@@ -1,43 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/davi-fernandesx/sistema-de-gestao-de-epi/auth"
-	"github.com/davi-fernandesx/sistema-de-gestao-de-epi/model"
+	"github.com/davi-fernandesx/sistema-de-gestao-de-epi/configs"
+	"github.com/gin-gonic/gin"
 )
 
 func main(){
 
 
 	
-	//router:= gin.Default()
+	router:= gin.Default()
 
-	//_, err:= configs.InitAplicattion()
-	//if err != nil {
-
-	//	log.Fatal(err)
-    //	}
-
-
-	//router.Run(":8080")
-
-	a:= model.Login{
-		Nome: "davi",
-		Senha: "uee4n43jkj",
-	}
-
-	fmt.Println(a.Nome, a.Senha)
-
-	h, err:= auth.HashPassword(a.Senha)
+	_, err:= configs.InitAplicattion()
 	if err != nil {
-		log.Fatal(err)
+
+	log.Fatal(err)
 	}
 
-	i:= fmt.Sprintf("%x", h)
 
-	fmt.Println("hash", i)
+	router.Run(":8080")
+
 
 	
 
