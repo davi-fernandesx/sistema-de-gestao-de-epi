01f1cdc3580c68b2555bdd0b35f29ea149d50303
davi <davifernadesr@gmail.com>
2025-09-20 21:38:52 -0300

    adcionados novos erros

--- repository/errosRepository.go
+++ repository/errosRepository.go
@@ -13,3 +13,7 @@ var  ErrusuarioJaExistente = errors.New("usuario já cadastrado")
 var  ErrAoApagarUmLogin = errors.New("erro ao apagar login")
 var ErrLinhasAfetadas = errors.New( "erro ao verificar as linhas afetadas")
 var ErrUsuarioNaoEncontrado = errors.New("usuario não encontrado")
+var ErrConexaoDb = errors.New("erro ao se conectar com o banco de dados")
+var ErrFalhaAoEscanearDados = errors.New("erro ao escanear os dados")
+
+
