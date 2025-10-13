cd666fde48dd3c2c776c1dd4c9b113a6e2b75140
davi <davifernadesr@gmail.com>
2025-10-13 00:28:30 -0300

    Move erros para novo pacote e simplifica estrutura de erros

---
+++ errors/errosRepository.go
@@ -0,0 +1,21 @@
+package Errors
+
+import (
+
+	"errors"
+)
+
+var (
+
+	ErrConexaoDb = errors.New("erro ao se conectar com o banco de dados")
+	ErrFalhaAoEscanearDados = errors.New("erro ao escanear os dados")
+	ErrLinhasAfetadas = errors.New( "erro ao verificar as linhas afetadas")
+	ErrDadoIncompativel = errors.New("tipo de dado invalido")
+	ErrAoapagar = errors.New("erro ao apagar")
+	ErrAoIterar = errors.New("erro ao iterar")
+	ErrSalvar = errors.New("erro ao salvar")
+	ErrBuscarTodos = errors.New("erro ao buscar todo os itens")
+    ErrNaoEncontrado = errors.New(" não encontrado")
+	ErrInternal      = errors.New("erro interno do repositório")
+
+)
