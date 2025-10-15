24969e1ca13351eb0aa0f814cb0f52c2aab5cf25
davi <davifernadesr@gmail.com>
2025-10-14 22:59:11 -0300

    refatoração: Remove tipos de entrega obsoletos e reorganiza estruturas de dados para entrega e itens

--- model/entrega_epi.go
+++ model/entrega_epi.go
@@ -2,40 +2,36 @@ package model
 
 import "time"
 
-type EntregaEpiInserir struct {
-	ID                 int
-	ID_funcionario     int
-	ID_epi             int
-	Data_entrega       time.Time
-	Assinatura_Digital string
-	Quantidade         int
+
+
+
+type ItemParaEntrega struct {
+    ID_epi     int `json:"id_epi"`
+    ID_tamanho int `json:"id_tamanho"`
+    Quantidade int `json:"quantidade"`
 }
 
-type EntregaEpi struct {
-	Id                 int
-	IDfuncionario      int
-	NomeFuncionario    string
-	Funcao             string
-	Departamento       string
-	IDEpi              int
-	Nome               string
-	Fabricante         string
-	CA                 string
-	Descricao          string
-	DataFabricacao     time.Time
-	DataValidade       time.Time
-	DataValidadeCa     time.Time
-	IDprotecao         int
-	NomeProtecao       string
-	Data_entrega       time.Time
-	Assinatura_Digital string
-	Quantidade         int
+
+type EntregaParaInserir struct {
+    ID_funcionario     int               `json:"id_funcionario"`
+    Data_entrega       time.Time         `json:"data_entrega"`
+    Assinatura_Digital string            `json:"assinatura_digital"`
+    Itens              []ItemParaEntrega `json:"itens"`
 }
 
-type Entrega_epi_dto struct {
-	Funcionario        Funcionario_Dto `json:"funcionario"`
-	Epi                EpiDto          `json:"epi"`
-	Data_entrega       time.Time       `json:"data_entrega"`
-	Assinatura_Digital string          `json:"assinatura_digital"`
-	Quantidade         int             `json:"quantidade"`
+
+
+
+type ItemEntregueDto struct {
+    Epi        EpiDto     `json:"epi"`
+    Tamanho    TamanhoDto `json:"tamanho"`
+    Quantidade int        `json:"quantidade"`
+}
+
+type EntregaDto struct {
+    Id                 int               `json:"id"`
+    Funcionario        Funcionario_Dto   `json:"funcionario"`
+    Data_entrega       time.Time         `json:"data_entrega"`
+    Assinatura_Digital string            `json:"assinatura_digital"`
+    Itens              []ItemEntregueDto `json:"itens"`
 }
