d903b2952b4a73f8246f147095cfa383861b3a10
davi <davifernadesr@gmail.com>
2025-11-06 16:11:47 -0300

    feat: Atualiza estrutura de FuncionarioINserir para incluir matrícula como string e IDs como ponteiros

--- model/funcionario.go
+++ model/funcionario.go
@@ -1,26 +1,26 @@
 package model
 
 type FuncionarioINserir struct {
-	ID              int   
-	Nome            string 
-	ID_departamento int    
-	ID_funcao       int    
+	Nome            string
+	Matricula       string
+	ID_departamento *int
+	ID_funcao       *int
 }
 
 type Funcionario struct {
-
-	Id int
-	Nome string
+	Id              int
+	Nome            string
+	Matricula       int
 	ID_departamento int
-	Departamento string
-	ID_funcao int
-	Funcao string
+	Departamento    string
+	ID_funcao       int
+	Funcao          string
 }
-
 
 type Funcionario_Dto struct {
 	ID           int    `json:"id"`
-	Nome         string          `json:"nome"`
+	Nome         string `json:"nome"`
+	Matricula    int
 	Departamento DepartamentoDto `json:"departamento"`
 	Funcao       FuncaoDto       `json:"funcao"`
 }
