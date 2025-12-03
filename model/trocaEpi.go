bada904420656165a59277382d6548376f84e9a4
davi <davifernadesr@gmail.com>
2025-12-02 22:28:20 -0300

    atualizando campos da estrutura DevolucaoInserir e Devolucao para melhor consistência nos nomes

--- model/trocaEpi.go
+++ model/trocaEpi.go
@@ -3,32 +3,35 @@ package model
 import "time"
 
 type DevolucaoInserir struct {
-	IdFuncionario     int       `json:"id_funcionario"`
-	IdEpi             int       `json:"id_epi"`
-	IdMotivo          int       `json:"id_motivo"`
-	DataDevolucao     time.Time `json:"data_devolucao"`
-	Quantidade        int       `json:"quantidade"`
-	IdEpiNovo         int       `json:"id_novo_epi"`
-	IdTamanhoNovo     int       `json:"tamanhoEpi_novo"`
-	AssinaturaDigital string    `json:"assinatura_digital"`
+	IdFuncionario       int       `json:"id_funcionario"`
+	IdEpi               int       `json:"id_epi"`
+	IdMotivo            int       `json:"id_motivo"`
+	DataDevolucao       time.Time `json:"data_devolucao"`
+	QuantidadeADevolver int       `json:"quantidade_a_devolver"`
+	NovaQuantidade      int       `json:"nova_quantidade"`
+	IdEpiNovo           int       `json:"id_novo_epi"`
+	IdTamanhoNovo       int       `json:"tamanhoEpi_novo"`
+	AssinaturaDigital   string    `json:"assinatura_digital"`
 }
 
 type Devolucao struct {
-	Id                int
-	DataEntrega       time.Time
-	Id_funcionario    int
-	NomeFuncionario   string
-	Id_departamento   int
-	Departamento      string
-	Id_funcao         int
-	Funcao            string
-	ID_epiTroca       int
-	NomeEpiTroca      string
-	FabricanteTroca   string
-	CAtroca           string
-	IdMotivo          int
-	Motivo            string
-	AssinaturaDigital string
+	Id                  int
+	DataEntrega         time.Time
+	Id_funcionario      int
+	NomeFuncionario     string
+	Id_departamento     int
+	Departamento        string
+	Id_funcao           int
+	Funcao              string
+	ID_epiTroca         int
+	NomeEpiTroca        string
+	FabricanteTroca     string
+	CAtroca             string
+	IdMotivo            int
+	Motivo              string
+	QuantidadeADevolver int
+	NovaQuantidade      int
+	AssinaturaDigital   string
 
 	ID_epiNovo int
 	NomeEpi    string
@@ -39,14 +42,15 @@ type Devolucao struct {
 }
 
 type DevolucaoDto struct {
-	Id                int             `json:"id"`
-	IdFuncionario     Funcionario_Dto `json:"id_funcionario"`
-	IdEpi             EpiDto          `json:"id_epi"`
-	MotivoDevolucao   DevolucaoEpiDto `json:"motivoDaDevolucao"`
-	DataDevolucao     time.Time       `json:"dataDevolucao"`
-	AssinaturaDigital string          `json:"assinatura_digital"`
+	Id                  int             `json:"id"`
+	IdFuncionario       Funcionario_Dto `json:"id_funcionario"`
+	IdEpi               EpiDto          `json:"id_epi"`
+	MotivoDevolucao     DevolucaoEpiDto `json:"motivoDaDevolucao"`
+	DataDevolucao       time.Time       `json:"dataDevolucao"`
+	QuantidadeADevolver int             `json:"quantidade_a_devolver"`
+	AssinaturaDigital   string          `json:"assinatura_digital"`
 
-	IdEpiNovo  EpiDto     `json:"id_novo_epi"`
-	Tamanho    TamanhoDto `json:"tamanho"`
-	Quantidade int        `json:"quantidade"`
+	IdEpiNovo      EpiDto     `json:"id_novo_epi"`
+	Tamanho        TamanhoDto `json:"tamanho"`
+	NovaQuantidade int        `json:"quantidade_nova"`
 }
