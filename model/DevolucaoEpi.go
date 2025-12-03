5d20b7fc44a0288f3caeb9c722827e1f8052aefd
davi <davifernadesr@gmail.com>
2025-12-02 22:29:50 -0300

    ajustando a formatação do código na estrutura DevolucaoEpiDto e na constante Numeracao_ou_tamanho_errado para melhor legibilidade

--- model/DevolucaoEpi.go
+++ model/DevolucaoEpi.go
@@ -6,14 +6,14 @@ type DevolucaoEpi struct {
 }
 
 type DevolucaoEpiDto struct {
-	Id int `json:"id"`
+	Id     int             `json:"id"`
 	Motivo MotivoDevolucao `json:"motivo"`
 }
 
 type MotivoDevolucao string
 
 const (
-	Numeracao_ou_tamanho_errado                  MotivoDevolucao = "Numeração ou tamanho errado"
+	Numeracao_ou_tamanho_errado       MotivoDevolucao = "Numeração ou tamanho errado"
 	Substituição_por_Desgaste_ou_Dano MotivoDevolucao = "Substituição por Desgaste ou Dano"
 	Data_de_validade_Vencida          MotivoDevolucao = "Vencimento da validade ou do CA"
 	Mudança_de_Função_ou_Setor        MotivoDevolucao = "Mudança de Função ou Setor"
