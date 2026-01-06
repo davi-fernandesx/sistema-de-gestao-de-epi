1bc48948596217187d8eccf7733ab21f89c6180a
davi <davifernadesr@gmail.com>
2026-01-05 22:19:26 -0300

    saervice de entregas

---
+++ service/entrega/entregaEpiService.go
@@ -0,0 +1,135 @@
+package entrega
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+
+	"strings"
+	"time"
+
+	Errors "github.com/davi-fernandesx/sistema-de-gestao-de-epi/errors"
+	"github.com/davi-fernandesx/sistema-de-gestao-de-epi/model"
+)
+
+//go:generate mockery --name=EntregaInterface --output=mocks --outpkg=mocks
+type EntregaInterface interface {
+	Addentrega(ctx context.Context, model model.EntregaParaInserir) error
+	BuscaEntrega(ctx context.Context, id int) (*model.EntregaDto, error)
+	BuscaEntregaPorIdFuncionario(ctx context.Context, idFuncionario int) ([]*model.EntregaDto, error)
+	BuscaEntregaPorIdFuncionarioCanceladas(ctx context.Context, idFuncionario int) ([]*model.EntregaDto, error)
+	BuscaTodasEntregas(ctx context.Context) ([]*model.EntregaDto, error)
+	BuscaTodasEntregasCanceladas(ctx context.Context) ([]*model.EntregaDto, error)
+	BuscaEntregaCancelada(ctx context.Context, id int) (*model.EntregaDto, error)
+	CancelarEntrega(ctx context.Context, id int) error
+}
+
+type EntregaService struct {
+	EntregaRepo EntregaInterface
+}
+
+func NewEntregaService(repo EntregaInterface) *EntregaService {
+
+	return &EntregaService{
+		EntregaRepo: repo,
+	}
+}
+
+var (
+	errDataMenor           = errors.New("A data de entrada não pode ser menor que hoje")
+	ErrFalhaNoBancoDeDados = errors.New("falha no banco de dados")
+	ErrId                  = errors.New("id invalido")
+	ErrNaoEncontrado       = errors.New("entrega não encontrada")
+	ErrInterno          = errors.New("erro interno do sistema")
+)
+
+func (e *EntregaService) SalvarEntrega(ctx context.Context, model model.EntregaParaInserir) error {
+
+	model.Assinatura_Digital = strings.TrimSpace(model.Assinatura_Digital)
+
+	hoje := time.Now().Truncate(24 * time.Hour)
+
+	if model.Data_entrega.Time().Truncate(24 * time.Hour).Before(hoje) {
+
+		return errDataMenor
+	}
+
+	err := e.EntregaRepo.Addentrega(ctx, model)
+	if err != nil {
+
+		log.Printf("erro ao salvar entrega: %v", err)
+		return ErrFalhaNoBancoDeDados
+
+	}
+
+	return nil
+}
+
+func (e *EntregaService) ListaEntrega(ctx context.Context, id int) (model.EntregaDto, error) {
+
+	if id <= 0 {
+
+		return model.EntregaDto{}, ErrId
+	}
+
+	entrega, err := e.EntregaRepo.BuscaEntrega(ctx, id)
+	if err != nil {
+
+		if errors.Is(err, Errors.ErrBuscarTodos) {
+
+			return model.EntregaDto{}, ErrNaoEncontrado
+		}
+
+		return model.EntregaDto{}, fmt.Errorf("falha interna: %w", ErrInterno)
+	}
+
+	if entrega == nil {
+
+		return model.EntregaDto{}, ErrInterno
+	}
+
+	return *entrega, nil
+}
+
+func (e *EntregaService) ListarTodasEntregas(ctx context.Context) ([]*model.EntregaDto, error) {
+
+	entregas, err := e.EntregaRepo.BuscaTodasEntregas(ctx)
+	if err != nil {
+
+		if errors.Is(err, Errors.ErrBuscarTodos) {
+
+			return []*model.EntregaDto{}, nil
+		}
+
+		return []*model.EntregaDto{}, fmt.Errorf("falha interna: %w", err)
+	}
+
+	if entregas == nil {
+		return []*model.EntregaDto{}, nil
+	}
+
+	return entregas, nil
+
+}
+
+func (e *EntregaService) CancelarEntrega(ctx context.Context, id int) error {
+
+	if id <= 0 {
+
+		return ErrId
+	}
+
+	err := e.EntregaRepo.CancelarEntrega(ctx, id)
+	if err != nil {
+
+		if errors.Is(err, Errors.ErrInternal) {
+
+			return ErrId
+		}
+
+		return fmt.Errorf("erro crítico ao buscar entrada ID %d: %v", id, err)
+	}
+
+	return nil
+}
