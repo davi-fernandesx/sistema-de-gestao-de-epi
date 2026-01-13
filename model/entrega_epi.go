2c515460ae00357074ec438d7598ad2b7ff2f601
davi <davifernadesr@gmail.com>
2026-01-12 23:02:33 -0300

    binding feitona model

--- model/entrega_epi.go
+++ model/entrega_epi.go
@@ -8,23 +8,22 @@ import (
 )
 
 type ItemParaInserir struct {
-	ID_epi         int             `json:"id_epi" binding:"requered,numeric"`
-	ID_tamanho     int             `json:"id_tamanho" binding:"required,numeric"`
-	Quantidade     int             `json:"quantidade" binding:"required,numeric,gt=0"`
-	Valor_unitario decimal.Decimal `json:"valor_unitario" binding:"required,gt=0"`
+	ID_epi           int64             `json:"id_epi" binding:"required,numeric"`
+	ID_tamanho       int64             `json:"id_tamanho" binding:"required,numeric"`
+	Quantidade       int             `json:"quantidade" binding:"required,numeric,gt=0"`
+	Valor_unitario   decimal.Decimal `json:"valor_unitario" binding:"required,gt=0"`
 }
 
 type EntregaParaInserir struct {
-	ID_funcionario     int               `json:"id_funcionario" binding:"requered,numeric"`
-	Data_entrega       configs.DataBr    `json:"data_entrega" binding:"requered"`
-	Assinatura_Digital string            `json:"assinatura_digital" binding:"requered"`
-	Itens              []ItemParaInserir `json:"itens" binding:"requered,min=1,dive"`
-	Id_troca           sql.NullInt64     `json:"-"`
+	ID_funcionario     int64               `json:"id_funcionario" binding:"required,numeric"`
+	Data_entrega       configs.DataBr    `json:"data_entrega" binding:"required"`
+	Assinatura_Digital string            `json:"assinatura_digital" binding:"required"`
+	Itens              []ItemParaInserir `json:"itens" binding:"required,min=1,dive"`
+	Id_troca           sql.NullInt64     
 }
 
-
 type ItemEntregueDto struct {
-	Id            int             `json:"id"`
+	Id            int64             `json:"id"`
 	Epi           EpiDto          `json:"epi"`
 	Tamanho       TamanhoDto      `json:"tamanho"`
 	Quantidade    int             `json:"quantidade"`
@@ -32,7 +31,7 @@ type ItemEntregueDto struct {
 }
 
 type EntregaDto struct {
-	Id                 int               `json:"id"`
+	Id                 int64              `json:"id"`
 	Funcionario        Funcionario_Dto   `json:"funcionario"`
 	Data_entrega       configs.DataBr    `json:"data_entrega"`
 	Assinatura_Digital string            `json:"assinatura_digital"`
