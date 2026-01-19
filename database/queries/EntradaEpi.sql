68451e0ef3a091f5f6060f7a54e1733b23f0f2bc
davi <davifernadesr@gmail.com>
2026-01-18 21:39:44 -0300

    mudando allguns filtros na busca de todas as entradas

--- database/queries/EntradaEpi.sql
+++ database/queries/EntradaEpi.sql
@@ -5,6 +5,7 @@ INSERT INTO entrada_epi (
 ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);
 
 -- name: ListarEntradas :many
+-- name: ListarEntradas :many
 SELECT 
     ee.id, ee.IdEpi, e.nome as epi_nome, e.fabricante, e.CA, e.descricao as epi_descricao,
     ee.data_fabricacao, ee.data_validade, e.validade_CA,
@@ -17,13 +18,39 @@ INNER JOIN epi e ON ee.IdEpi = e.id
 INNER JOIN tipo_protecao tp ON e.IdTipoProtecao = tp.id
 INNER JOIN tamanho t ON ee.IdTamanho = t.id
 WHERE 
-    (sqlc.arg('canceladas')::boolean IS FALSE AND ee.cancelada_em IS NULL) OR
-    (sqlc.arg('canceladas')::boolean IS TRUE AND ee.cancelada_em IS NOT NULL)
-AND (sqlc.narg('id_epi')::int IS NULL OR ee.IdEpi = sqlc.narg('id_epi'))
-AND (sqlc.narg('id_entrada')::int IS NULL OR ee.id = sqlc.narg('id_entrada'));
+    (
+        (@canceladas::boolean IS FALSE AND ee.cancelada_em IS NULL) OR
+        (@canceladas::boolean IS TRUE AND ee.cancelada_em IS NOT NULL)
+    )
+    AND (sqlc.narg('id_epi')::int IS NULL OR ee.IdEpi = sqlc.narg('id_epi'))
+    AND (sqlc.narg('id_entrada')::int IS NULL OR ee.id = sqlc.narg('id_entrada'))
+    AND (sqlc.narg('data_inicio')::date IS NULL OR ee.data_entrada >= sqlc.narg('data_inicio'))
+    AND (sqlc.narg('data_fim')::date IS NULL OR ee.data_entrada <= sqlc.narg('data_fim'))
+    AND (sqlc.narg('nota_fiscal')::text IS NULL OR ee.nota_fiscal_numero ILIKE '%' || sqlc.narg('nota_fiscal') || '%') -- Busca por NF
+ORDER BY ee.data_entrada DESC
+LIMIT $1 OFFSET $2;
 
 -- name: CancelarEntrada :execrows
-UPDATE entrada_epi
-SET cancelada_em = NOW(),
-    ativo = FALSE
-WHERE id = $1 AND ativo = TRUE;
+-- name: CancelarEntrada :execrows
+UPDATE entrada_epi 
+SET 
+    cancelada_em = NOW(), 
+    ativo = FALSE 
+WHERE id = $1 
+  AND cancelada_em IS NULL 
+  AND quantidadeAtual = quantidade; -- Garante que nada foi usado
+
+-- name: ContarEntradas :one
+-- name: ContarEntradasFiltradas :one
+SELECT COUNT(*) 
+FROM entrada_epi ee
+WHERE 
+    (
+        (@canceladas::boolean IS FALSE AND ee.cancelada_em IS NULL) OR
+        (@canceladas::boolean IS TRUE AND ee.cancelada_em IS NOT NULL)
+    )
+    AND (sqlc.narg('id_epi')::int IS NULL OR ee.IdEpi = sqlc.narg('id_epi'))
+    AND (sqlc.narg('id_entrada')::int IS NULL OR ee.id = sqlc.narg('id_entrada'))
+    AND (sqlc.narg('data_inicio')::date IS NULL OR ee.data_entrada >= sqlc.narg('data_inicio'))
+    AND (sqlc.narg('data_fim')::date IS NULL OR ee.data_entrada <= sqlc.narg('data_fim'))
+    AND (sqlc.narg('nota_fiscal')::text IS NULL OR ee.nota_fiscal_numero ILIKE '%' || sqlc.narg('nota_fiscal') || '%');-- adicione os mesmos filtros aqui
