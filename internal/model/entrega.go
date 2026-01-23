9bac4fa843500b73709cde841102c336217cb338
davi <davifernadesr@gmail.com>
2026-01-22 23:08:11 -0300

    retirado o valor unitario, nao faz sentido

--- internal/model/entrega.go
+++ internal/model/entrega.go
@@ -6,16 +6,16 @@ import (
 )
 
 type ItemParaInserir struct {
-	ID_epi         int64           `json:"id_epi" binding:"required"`
-	ID_tamanho     int64           `json:"id_tamanho" binding:"required"`
-	Quantidade     int             `json:"quantidade" binding:"required,numeric,gt=0"`
-	Valor_unitario decimal.Decimal `json:"valor_unitario" binding:"required,gt=0"`
+	ID_epi     int64 `json:"id_epi" binding:"required"`
+	ID_tamanho int64 `json:"id_tamanho" binding:"required"`
+	Quantidade int   `json:"quantidade" binding:"required,numeric,gt=0"`
 }
 
 type EntregaParaInserir struct {
 	ID_funcionario     int64             `json:"id_funcionario" binding:"required"`
 	Id_user            int               `json:"id_user" binding:"required,numeric"`
 	Data_entrega       configs.DataBr    `json:"data_entrega" binding:"required"`
+	IdTroca            *int              `json:"idTroca"`
 	Assinatura_Digital string            `json:"assinatura_digital" binding:"required"`
 	Itens              []ItemParaInserir `json:"itens" binding:"required,min=1,dive"`
 }
