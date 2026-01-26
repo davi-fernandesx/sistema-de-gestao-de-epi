488d9442e8bfe78867396db94eecd29bd4241f04
davi <davifernadesr@gmail.com>
2026-01-26 20:45:01 -0300

    adiciona middleware de autenticação JWT e configuração CORS

---
+++ middleware/cors.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+
+func CorsConfig() gin.HandlerFunc {
+
+	return cors.New(cors.Config{
+
+		//da onde meu back-end aceita requisições
+		AllowOrigins: []string{
+			"https://sgepi-front-end.vercel.app", //homologação
+			"http://localhost:3000", //local
+		},
+
+		AllowMethods: []string{
+			"POST","PUT","GET","PATCH", "DELETE", "OPTIONS",
+		},//metados permitidos
+
+		AllowHeaders: []string{
+
+			"Origin",
+			"Content-type",
+			"Accept",
+			"Authorization",
+			"X-Request-With",
+		},//cabeçalhos que o front pode enviar
+
+		ExposeHeaders: []string{
+
+			"Content-Length",
+		}, //informações adicionais que o front pode ler
+
+		AllowCredentials: true,
+		MaxAge: 9 * time.Hour, // ate 9 horas para o crs fazer a verificação de novo
+	})
+}
