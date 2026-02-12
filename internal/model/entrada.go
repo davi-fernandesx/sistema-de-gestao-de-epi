533df8033964a7fbc12283226ae738f1ba920897
davi <davifernadesr@gmail.com>
2026-02-11 23:12:59 -0300

    modicando o o user em entrada dto e tuirando algumas validações desnecessaria

--- internal/model/entrada.go
+++ internal/model/entrada.go
@@ -13,24 +13,25 @@ type EntradaEpiInserir struct {
 	Quantidade_Atual   int             `json:"quantidade_Atual" binding:"required,numeric,gt=0"`
 	Quantidade         int             `json:"quantidade" binding:"required,numeric,gt=0"`
 	DataFabricacao     configs.DataBr  `json:"data_fabricacao" binding:"required"`
-	DataValidade       configs.DataBr  `json:"data_validade" binding:"required,gtfield=DataFabricacao"`
+	DataValidade       configs.DataBr  `json:"data_validade" binding:"required"`
 	Lote               string          `json:"lote" binding:"required,numeric,max=6"`
 	Fornecedor         string          `json:"fornecedor" binding:"required,max=50"`
-	Nota_fiscal_serie  string          `json:"notaFicalSerie" binding:"required,max=20,numeric"`
+	Nota_fiscal_serie  string          `json:"notaFiscalSerie" binding:"required,max=20,numeric"`
 	Nota_fiscal_numero string          `json:"notaFiscalNumero" binding:"required,max=10,numeric"`
-	ValorUnitario      decimal.Decimal `json:"valorUnitario" binding:"required,gte=0"`
+	ValorUnitario      decimal.Decimal `json:"valorUnitario" binding:"required"`
 }
 
 type EntradaEpiDto struct {
-	ID                 int             `json:"id"`
-	Id_user            int             `json:"id_user"`
-	Epi                EpiDto          `json:"epi"`
-	Data_entrada       configs.DataBr  `json:"data_entrada"`
-	Quantidade         int             `json:"quantidade"`
-	Quantidade_Atual   int             `json:"quantidade_Atual"`
-	Lote               string          `json:"lote"`
-	Fornecedor         string          `json:"fornecedor"`
-	Nota_fiscal_serie  string          `json:"notaFicalSerie"`
-	Nota_fiscal_numero string          `json:"notaFiscalNumero"`
-	ValorUnitario      decimal.Decimal `json:"valor_unitario"`
+	ID                         int                 `json:"id"`
+	UsuarioEntrada             RecuperaUserEntrada `json:"usuario"`
+	Epi                        EpiDto              `json:"epi"`
+	Data_entrada               configs.DataBr      `json:"data_entrada"`
+	Quantidade                 int                 `json:"quantidade"`
+	Quantidade_Atual           int                 `json:"quantidade_Atual"`
+	Lote                       string              `json:"lote"`
+	Fornecedor                 string              `json:"fornecedor"`
+	Nota_fiscal_serie          string              `json:"notaFicalSerie"`
+	Nota_fiscal_numero         string              `json:"notaFiscalNumero"`
+	UsuarioEntradaCancelamento RecuperaUserEntrada `json:"usuario_Cancelamento"`
+	ValorUnitario              decimal.Decimal     `json:"valor_unitario"`
 }
