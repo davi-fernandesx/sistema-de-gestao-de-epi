68462c609ddb4e5617953c2c2209fb6acf918f48
davi <davifernadesr@gmail.com>
2026-02-24 19:11:24 -0300

    Fix: Refatora configuração CORS para usar AllowOriginFunc e corrige formatação de cabeçalhos

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,38 +1,51 @@
 package middleware
 
 import (
+	"strings"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-
 func CorsConfig() gin.HandlerFunc {
 
 	return cors.New(cors.Config{
 
 		//da onde meu back-end aceita requisições
-		AllowOrigins: []string{
-			"https://sgepi-front-end.vercel.app", //homologação
-			"http://localhost:3000", //local
-			"https://radaptech.com.br",//homologação
-			"https://www.radaptech.com.br",
+		AllowOriginFunc: func(origin string) bool {
+			if origin == "http://localhost:3000" || origin == "https://sgepi-front-end.vercel.app" {
+				return true
+			}
+
+			//dominio principal
+			if origin == "https://radaptech.com.br/" || origin == "https://www.radaptech.com.br/" {
+				//
+				return true
+			}
+
+			//permitir qualquer subdominio ex: (frigorificosaojoao.radaptech.com.br)
+
+			if strings.HasSuffix(origin, ".radaptech.com.br"){
+				return true
+			}
+
+			return false
 		},
 
 		AllowMethods: []string{
-			"POST","PUT","GET","PATCH", "DELETE", "OPTIONS",
-		},//metados permitidos
+			"POST", "PUT", "GET", "PATCH", "DELETE", "OPTIONS",
+		}, //metados permitidos
 
 		AllowHeaders: []string{
 
 			"Origin",
-			"Content-type",
+			"Content-Type",
 			"Accept",
 			"Authorization",
 			"X-Requested-With",
 			"X-Tenant-ID",
-		},//cabeçalhos que o front pode enviar
+		}, //cabeçalhos que o front pode enviar
 
 		ExposeHeaders: []string{
 
@@ -40,6 +53,6 @@ func CorsConfig() gin.HandlerFunc {
 		}, //informações adicionais que o front pode ler
 
 		AllowCredentials: true,
-		MaxAge: 9 * time.Hour, // ate 9 horas para o crs fazer a verificação de novo
+		MaxAge:           9 * time.Hour, // ate 9 horas para o crs fazer a verificação de novo
 	})
 }
